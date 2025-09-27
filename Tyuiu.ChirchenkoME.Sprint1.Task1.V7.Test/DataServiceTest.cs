dca8880189ce7736c9f09762e135ab7ad7a4a675
Administrator <Administrator@WIN-M6NM0GBOMH4>
2025-09-27 13:47:30 +0500

    Add project files.

---
+++ Tyuiu.ChirchenkoME.Sprint1.Task1.V7.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.ChirchenkoME.Sprint1.Task1.V7.Lib;
+
+namespace Tyuiu.ChirchenkoME.Sprint1.Task1.V7.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 1.0;
+            double y = 2.0;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(1.66666666667, res);
+        }
+    }
+}
